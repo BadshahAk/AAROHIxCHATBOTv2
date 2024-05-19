79997e68c0f83379c0de03f9d185c176e7b68ea0
⫷ Aɳαɳԃ Sԋαɾɱα ⫸ <91930171+BadshahAk@users.noreply.github.com>
2024-05-19 07:21:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "29695886"
-API_HASH = "14ca35d1ae7a732d32f2f28c83ed54db"
+API_ID = "8753950"
+API_HASH = "d6b6332c27adb1ab40d4009f9bead12c"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "7136886425"
+OWNER_ID = "7155067431"
 SUPPORT_GRP = "Kanha_KI_Nagri"
 UPDATE_CHNL = "LegendGod_Anand"
 OWNER_USERNAME = "unofficial_killer"
